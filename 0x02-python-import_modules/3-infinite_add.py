2335457c944c08f443d9d2a4423121674d4a2e69
profe-ssor <kyerematengcollins93@gmail.com>
2023-12-27 09:53:24 -0800

    imports

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    def add_arguments():
+        arguments = sys.argv[1:]
+        result = sum(int(arg) for arg in arguments)
+        print(result)
+
+    add_arguments()
