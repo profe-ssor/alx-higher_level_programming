28f76ba98da8bbfd80f820149f53adf092d2ce75
profe-ssor <kyerematengcollins93@gmail.com>
2024-02-27 08:31:44 +0000

    first

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Square"
